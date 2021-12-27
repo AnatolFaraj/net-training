dda995233b18457ff8fb4de4610c8452052423d7
Faraj Anatol <bestvicious.w@gmail.com>
2021-12-27 15:16:52 +0300

    implemented GetFibonacciSequence

--- 03-Collections/Collections/Collections.cs
+++ 03-Collections/Collections/Collections.cs
@@ -38,35 +38,33 @@ namespace Collections.Tasks
         public static IEnumerable<int> GetFibonacciSequence(int count) 
         {
 
-            int [] sequence = new int [count];
-
-            if (count == 0)
-            {
-                
-                sequence = null;
-                
-            }
-            
-
             if (count < 0)
             {
                 throw new ArgumentException();
             }
-            else if (count !=0 )
+
+            int[] array = new int[count];
+
+
+            if (count == 0)
             {
-                sequence[0] = 1;
-                sequence[1] = 1;
+                return new int[0];
+            }
+            else
+            {
+                array[0] = 1;
+                array[1] = 1;
 
-                for (int i = 2; i < count; i++)
+                for (int i = 2; i < array.Length; i++)
                 {
-                    sequence[i] = sequence[i - 2] + sequence[i - 1];
-
+                    array[i] = array[i - 1] + array[i - 2];
                 }
+
             }
 
-            return sequence;
+            return array;
 
-            
+
 
         }
 
